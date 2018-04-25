417d0bec7828c5adc770bf42d73bf41b2c730140
unknown <1336250263@qq.com>
2018-04-25 16:06:15 +0800

    converted into groupby mode and added normalize speed framework

--- extract_data.py
+++ extract_data.py
@@ -25,44 +25,52 @@ class Data:
     def load_data(self):
         self.data_from_file = pd.read_csv(self.path)
         self.data_from_file.columns = self.columns
-        self.data_from_file = self.data_from_file.groupby("TERMINALNO")
 
     def split_data(self):
-        tag = 0
-        terminal_no = self.data_from_file.ix[0,"TERMINALNO"]
-        start = 0
-        end = 0
-        while tag<self.data_from_file.shape[0]:
-            if self.data_from_file.ix[tag,"TERMINALNO"] != terminal_no:
-                end = tag-1
-                self.process_personal(start,end)
-                start = tag
-                terminal_no = self.data_from_file.ix[tag,"TERMINALNO"]
-            tag+=1
-            if tag==self.data_from_file.shape[0]:
-                end = tag - 1
-                self.process_personal(start, end)
+        # tag = 0
+        # terminal_no = self.data_from_file.ix[0,"TERMINALNO"]
+        # start = 0
+        # end = 0
+        # while tag<self.data_from_file.shape[0]:
+        #     if self.data_from_file.ix[tag,"TERMINALNO"] != terminal_no:
+        #         end = tag-1
+        #         self.process_personal(start,end)
+        #         start = tag
+        #         terminal_no = self.data_from_file.ix[tag,"TERMINALNO"]
+        #     tag+=1
+        #     if tag==self.data_from_file.shape[0]:
+        #         end = tag - 1
+        #         self.process_personal(start, end)
 
+        for terminal_no,personal_data in self.data_from_file.groupby("TERMINALNO"):
+            self.process_personal(terminal_no,personal_data)
         del self.data_from_file
         gc.collect()
 
-    def process_personal(self,start,end):
-        personal_data = self.data_from_file.ix[start:end,columns]
+    def process_personal(self, terminal_no, personal_data):
+        # personal_data = self.data_from_file.ix[start:end,columns]
         # personal_data = personal_data.sort_values(by=["TIME"],ascending=[1])
-        nos = []
-        # testing part to see if two or more terminal no in a group
-        for no in personal_data.ix[:, "TERMINALNO"]:
-            if not no in nos:
-                nos.append(no)
-        if len(nos) == 1:
-            self.personal_datas[nos[0]] = dict()
-            self.personal_datas[nos[0]]["start"] = start
-            self.personal_datas[nos[0]]["end"] = end
-            personal_array = np.array(personal_data.ix[:,valid])
-            self.personal_datas[nos[0]]["original"] = personal_data
-            self.personal_datas[nos[0]]["data"] = personal_array[:,:-1]
-            self.personal_datas[nos[0]]["y"] = personal_array[:,-1]
 
+        personal_data = personal_data.sort_values(by=["TRIP_ID", "TIME"], ascending=[1, 1])
+        self.personal_datas[terminal_no] = dict()
+        personal_array = np.array(personal_data.ix[:,valid])
+        self.personal_datas[terminal_no]["original"] = personal_data
+        self.personal_datas[terminal_no]["data"] = personal_array[:,:-1]
+        self.personal_datas[terminal_no]["y"] = personal_array[:,-1]
+        normed_speed = self.normalize_speed(personal_data)
+        # for trip_id,trip_data in personal_data.groupby("TRIP_ID"):
+        #     self.personal_datas[terminal_no][trip_id] = trip_data
+
+    def normalize_speed(self,personal_data):
+        original_speed = np.array(personal_data.ix[:,"SPEED"])
+        normalized_speed_data = self.normalize_min_max(original_speed)
+        normalize_speed = pd.DataFrame(data=normalized_speed_data,index=personal_data.index,columns=["SPEED_NORMED"])
+        return normalize_speed
+
+    def normalize_min_max(self,data):
+        # min max normalize function :
+
+        return data
 
 if __name__ == "__main__":
     data = Data(path,columns)
